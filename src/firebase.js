412ff11e3bd93efc12796677bd03b0508785fa27
ThinkBiz-H <thinkbizh@gmail.com>
2026-04-21 09:04:38 +0530

    Remove firebase configuration from firebase.js
    Removed firebase configuration details from the file.

--- src/firebase.js
+++ src/firebase.js
@@ -4,16 +4,6 @@ import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAvp0Muo1g8zPgIfdWOvU4aWilTS7nh9Qs",
-  authDomain: "thinkbiz-website.firebaseapp.com",
-  databaseURL: "https://thinkbiz-website-default-rtdb.firebaseio.com",
-  projectId: "thinkbiz-website",
-  storageBucket: "thinkbiz-website.firebasestorage.app",
-  messagingSenderId: "439084380154",
-  appId: "1:439084380154:web:e9161f089927c9abadbe43",
-  measurementId: "G-7VNSQ0KB1V",
-};
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
